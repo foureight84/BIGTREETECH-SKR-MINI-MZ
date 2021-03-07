9c34290c17f68fbc9e904166a1b5b0e65813da35
Khoa Truong <foureight84@gmail.com>
2021-03-07 15:52:56 -0800

    updated lin_advance value and bltouch value

--- SKR mini MZ V1.0/firmware/Marlin-2.0.7.2-SKR-mini-MZ/Marlin/Configuration.h
+++ SKR mini MZ V1.0/firmware/Marlin-2.0.7.2-SKR-mini-MZ/Marlin/Configuration.h
@@ -986,7 +986,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -41.55, -16.5, 2 }
+#define NOZZLE_TO_PROBE_OFFSET { -40, -16, 2 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
